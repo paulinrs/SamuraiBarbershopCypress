972f02c53c41e9068d7edf0f12e33e8d67c8828d
ruby --version <paulinn.rs@gmail.com>
2022-09-03 15:21:44 -0300

    configurações do banco de dados no arquivo cypress.json

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -22,14 +22,9 @@ module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
 
-  const pool = new Pool({
-    host: 'batyr.db.elephantsql.com',
-    user: 'aqesebwc',
-    password: '6_MWSSVCr1BpQdI4ltsA1nJ9wSZNGQwL',
-    database: 'aqesebwc',
-    port: 5432
-
-  })
+ const configJson = require(config.configFile)
+ 
+  const pool = new Pool(configJson.dbConfig)
 
   on('task', {
     removeUser(email) {
